3106a7c40ef6cda1a8ecba2e3802aa11df13336a
David Pinarto <davidpinarto90@gmail.com>
2023-06-29 21:31:31 +0800

    create dashboard page

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
@@ -41,7 +43,16 @@ module.exports = {
     new HtmlWebpackPlugin({
       title: 'Dashboard',
       filename: 'index.html',
-      template: path.resolve(__dirname, 'src/views/index.html'),
+      template: path.resolve(__dirname, 'src/views/dashboard.html'),
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
     }),
 
     new CleanWebpackPlugin(),
